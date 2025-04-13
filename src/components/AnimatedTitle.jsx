cffa5b5aaea3312b54fd637501d30dc8d81c2d90
Shaikat0079 <baruashaikat00790@gmail.com>
2025-04-13 17:06:56 +0930

    Added About section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+    return (
+        <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+            DisCover the World's <br /> Largest virtual Adventure!
+        </div>
+    )
+}
+
+export default AnimatedTitle    
